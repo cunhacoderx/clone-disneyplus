78c29a1221bf3cf011dec801a688e42c0f2f883f
cunhacoderx <leonardocunha.eng@outlook.com>
2025-05-04 23:08:02 -0300

    criando o hero e importanto a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-  return gulp.src('./src/styles/*.scss')
+  return gulp
+    .src('./src/main.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/css/'));
 }
 
-exports.default = styles;
-exports.watch = function () {
-  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+function images() {
+  return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
 }
+
+exports.default = gulp.parallel(styles, images);
+
+exports.watch = function () {
+  gulp.watch('./src/**/*.scss', gulp.parallel(styles));
+  gulp.watch('./src/images/**/*', gulp.parallel(images));
+};
